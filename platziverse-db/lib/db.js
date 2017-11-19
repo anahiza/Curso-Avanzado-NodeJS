39909d11f0da143b10b5778cdbac285cd3c42e44
Anahi <anahi.z.a@gmail.com>
2017-11-19 13:02:32 -0600

    Implementación de modelos con sequelize

---
+++ platziverse-db/lib/db.js
@@ -0,0 +1,9 @@
+'use strict'
+const Sequelize = require('sequelize')
+let sequelize = null
+module.exports = function setupDatabase (config) {
+  if (!sequelize) {
+    sequelize = new Sequelize(config)
+  }
+  return sequelize
+}
