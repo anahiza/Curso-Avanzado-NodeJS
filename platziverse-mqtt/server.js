5f286852f485e96b9d88db75a8b5ac094a4466f5
Anahi <anahi.z.a@gmail.com>
2017-12-27 16:39:02 -0600

    Como recibir mensajes

--- platziverse-mqtt/server.js
+++ platziverse-mqtt/server.js
@@ -6,17 +6,41 @@ const redis = require('redis')
 const chalk = require('chalk')
 
 const backend = {
-    type: 'redis',
-    redis,
-    return_buffers: true
+  type: 'redis',
+  redis,
+  return_buffers: true
 }
 const settings = {
-    port: 1883,
-    backend
+  port: 1883,
+  backend
 }
 
 const server = new mosca.Server(settings)
 
 server.on('ready', () => {
-    console.log(`${chalk.green('[platziverse-mqtt]')} server is running`)
+  console.log(`${chalk.green('[platziverse-mqtt]')} server is running`)
 })
+
+server.on('clientConnected', client => {
+  debug(`Client Connected: ${client.id}`)
+})
+
+server.on('clientDisconnected', client => {
+  debug(`Client Disconnected: ${client.id}`)
+})
+
+server.on('published', (packet, client) => {
+  debug(`Received: ${packet.topic}`)
+  debug(`Payload: ${packet.payload}}`)
+})
+
+server.on('error', handleFatalError)
+
+function handleFatalError(err){
+    console.log(`${chalk.red('[fatal error]')} ${err.message}`)
+    console.log(err.stack)
+    process.exit(1)
+}
+
+process.on('uncaughtException', handleFatalError)
+process.on('unhandledRejection', handleFatalError)
