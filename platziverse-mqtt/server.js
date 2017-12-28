bae4398047fecd65ac1471c0cfad847376f15a6b
Anahi <anahi.z.a@gmail.com>
2017-12-28 11:01:41 -0600

    Como integrar platziverse-db

--- platziverse-mqtt/server.js
+++ platziverse-mqtt/server.js
@@ -4,6 +4,7 @@ const debug = require('debug')('platziverse:mqtt')
 const mosca = require('mosca')
 const redis = require('redis')
 const chalk = require('chalk')
+const db = require('platziverse-db')
 
 const backend = {
   type: 'redis',
@@ -15,9 +16,24 @@ const settings = {
   backend
 }
 
+const config = {
+  database: process.env.DB_NAME || 'platziverse',
+  username: process.env.DB_USER || 'anahi',
+  password: process.env.DB_PASS || 'anahi',
+  host: process.env.DB_HOST || 'localhost',
+  dialect: 'postgres',
+  setup: false,
+  logging: s => debug(s)
+}
+
+let Agent, Metric
+
 const server = new mosca.Server(settings)
 
-server.on('ready', () => {
+server.on('ready', async () => {
+  const services = await db(config).catch(handleFatalError)
+  Agent = services.Agent
+  Metric = services.Metric
   console.log(`${chalk.green('[platziverse-mqtt]')} server is running`)
 })
 
@@ -36,10 +52,10 @@ server.on('published', (packet, client) => {
 
 server.on('error', handleFatalError)
 
-function handleFatalError(err){
-    console.log(`${chalk.red('[fatal error]')} ${err.message}`)
-    console.log(err.stack)
-    process.exit(1)
+function handleFatalError (err) {
+  console.error(`${chalk.red('[fatal error]')} ${err.message}`)
+  console.error(err.stack)
+  process.exit(1)
 }
 
 process.on('uncaughtException', handleFatalError)
