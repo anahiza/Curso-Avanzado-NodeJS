a93fe48752545f39c5040c1fbb9297461bc473e3
Anahi <anahi.z.a@gmail.com>
2017-12-29 14:49:56 -0600

    Como implementar un custom EventEmitter usando ES6 classes

---
+++ platziverse-agent/index.js
@@ -0,0 +1,10 @@
+'use strict'
+const EventEmitter = require('events')
+
+class PlatziverseAgent extends EventEmitter{
+    constructor(){
+        super()
+    }
+}
+
+module.exports = PlatziverseAgent
