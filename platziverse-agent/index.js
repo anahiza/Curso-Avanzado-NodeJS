e1a38ee143cc5dcbecd05c2c9fbc90e07b748dc5
Anahi <anahi.z.a@gmail.com>
2017-12-31 22:13:18 -0600

    Implementación del cliente mqtt

--- platziverse-agent/index.js
+++ platziverse-agent/index.js
@@ -1,22 +1,63 @@
 'use strict'
 const EventEmitter = require('events')
+const debug = require('debug')('platziverse:agent')
+const mqtt = require('mqtt')
+const defaults = require('defaults')
+const { parsePayload } = require('./utils')
+const uuid = require('uuid')
+
+const options = {
+  name: 'untitled',
+  username: 'platzi',
+  interval: 5000,
+  mqtt: {
+    host: 'mqtt://localhost'
+  }
+}
 
 class PlatziverseAgent extends EventEmitter {
   constructor (opts) {
     super()
     this._started = false
     this._timer = null
-    this._options = opts
+    this._options = defaults(opts, options)
+    this._client = null
+    this._agentId = null
   }
 
   connect () {
     if (!this._started) {
-      this._started = true
-      this.emit('connected')
       const opts = this._options
-      this._timer = setInterval(() => {
-        this.emit('agent/message', 'this is a message')
-      }, opts.inverval)
+      this._client = mqtt.connect(opts.mqtt.host)
+      this._started = true
+      this._client.subscribe('agent/message')
+      this._client.subscribe('agent/connected')
+      this._client.subscribe('agent/disconnected')
+      this._client.on('connect', () => {
+        this._agentId = uuid.v4()
+        this.emit('connected', this._agentId)
+        this._timer = setInterval(() => {
+          this.emit('agent/message', 'this is a message')
+        }, opts.inverval)
+      })
+
+      this._client.on('message', (topic, payload) => {
+        payload = parsePayload(payload)
+        let broadcast = false
+
+        switch (topic) {
+          case 'agent/connected':
+          case 'agent/disconnected':
+          case 'agent/message':
+            broadcast = payload && payload.agent && payload.agent.uuid != this._agentId
+            break
+        }
+        if (broadcast) {
+          this.emit(topic, payload)
+        }
+      })
+
+      this._client.on('error', () => this.disconnect())
     }
   }
 
