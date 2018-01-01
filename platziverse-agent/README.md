1fa391308a8427693824055d6cd40acc527500fd
Anahi <anahi.z.a@gmail.com>
2017-12-31 21:07:54 -0600

    Definiendo un timer de ejecución continua

---
+++ platziverse-agent/README.md
@@ -0,0 +1,17 @@
+# platziverse-agent
+
+## Usage
+
+``` js
+const PlatziverseAgent = require('platziverse-agent')
+const agent = new PlatziverseAgent({
+    interval: 2000
+})
+agent.connect()
+agent.on('agent/message', payload=> {
+    console.log(payload)
+})
+
+setTimeout(() => agent.disconnect(), 2000)
+
+```
--- platziverse-agent/index.js
+++ platziverse-agent/index.js
@@ -1,10 +1,32 @@
 'use strict'
 const EventEmitter = require('events')
 
-class PlatziverseAgent extends EventEmitter{
-    constructor(){
-        super()
+class PlatziverseAgent extends EventEmitter {
+  constructor (opts) {
+    super()
+    this._started = false
+    this._timer = null
+    this._options = opts
+  }
+
+  connect () {
+    if (!this._started) {
+      this._started = true
+      this.emit('connected')
+      const opts = this._options
+      this._timer = setInterval(() => {
+        this.emit('agent/message', 'this is a message')
+      }, opts.inverval)
     }
+  }
+
+  disconnect () {
+    if (this._started) {
+      clearInterval(this._timer)
+      this._started = false
+      this.emmit('disconnected')
+    }
+  }
 }
 
 module.exports = PlatziverseAgent
