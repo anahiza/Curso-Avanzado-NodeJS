f988f00a9c4f18b2e06c53cf8708d52a9a68697d
Anahi <anahi.z.a@gmail.com>
2018-01-14 13:04:22 -0600

    Implementación de un servidor básico con express

---
+++ platziverse-api/server.js
@@ -0,0 +1,13 @@
+'use strict'
+
+const http = require('http')
+const chalk = require('chalk')
+const express = require('express')
+
+const app = express()
+const server = http.createServer(app)
+const port = process.env.PORT || 3000
+
+server.listen(port, () => {
+    console.log(`${chalk.green('[platziverse-api]')} server listening on port ${port}`)
+})
