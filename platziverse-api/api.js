d379992b13a330e7a71d17d77cff539b4445a82d
Anahi <anahi.z.a@gmail.com>
2018-01-14 13:22:52 -0600

    Definición de rutas del API

---
+++ platziverse-api/api.js
@@ -0,0 +1,26 @@
+'use strict'
+const debug = require('debug')('platizverse:api:routes')
+const express = require('express')
+const api = express.Router()
+
+api.get('/agents', (req, res) => {
+  debug('A request has come to agents')
+  res.send({})
+})
+
+api.get('/agents/:uuid', (req, res) => {
+  const { uuid } = req.params
+  res.send({uuid})
+})
+
+api.get('/metrics/:uuid', (req, res) => {
+  const { uuid } = req.params
+  res.send({ uuid })
+})
+
+api.get('/metrics/:uuid/:type', (req, res) => {
+  const { uuid, type } = req.params
+  res.send({ uuid, type })
+})
+
+module.exports = api
--- platziverse-api/server.js
+++ platziverse-api/server.js
@@ -3,11 +3,14 @@
 const http = require('http')
 const chalk = require('chalk')
 const express = require('express')
+const api = require('./api')
 
 const app = express()
 const server = http.createServer(app)
 const port = process.env.PORT || 3000
 
+app.use('/api', api)
+
 server.listen(port, () => {
-    console.log(`${chalk.green('[platziverse-api]')} server listening on port ${port}`)
+  console.log(`${chalk.green('[platziverse-api]')} server listening on port ${port}`)
 })
