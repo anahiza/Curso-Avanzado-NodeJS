ab234fdc0f52598cfe0207a9ef8b9bd2811deb89
Anahi <anahi.z.a@gmail.com>
2018-03-25 17:27:21 -0600

    Implementación de Layout Básico con Blessed Contrib

--- platziverse-cli/platziverse.js
+++ platziverse-cli/platziverse.js
@@ -1,10 +1,30 @@
 #!/usr/bin/env node
- 
-const args = require('args')
- 
-args
-  .option('port', 'The port on which the app will be running', 3000)
-  .option('reload', 'Enable/disable livereloading')
-  .command('serve', 'Serve your static site', ['s'])
- 
-const flags = args.parse(process.argv)
+'use strict'
+/* eslint new-cap: "off" */
+
+const blessed = require('blessed')
+const contrib = require('blessed-contrib')
+
+const screen = blessed.screen()
+
+const grid = new contrib.grid({
+  rows: 1,
+  cols: 4,
+  screen
+})
+
+const tree = grid.set(0, 0, 1, 1, contrib.tree, {
+  label: 'Connected Agents'
+})
+const line = grid.set(0, 1, 1, 3, contrib.line, {
+  label: 'Metric',
+  showLegend: true,
+  minY: 0,
+  xPadding: 5
+})
+
+screen.key(['escape', 'q', 'C-c'], (ch, key) => {
+  process.exit(0)
+})
+
+screen.render()
